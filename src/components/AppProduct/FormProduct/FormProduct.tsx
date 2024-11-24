37504884e739671f6ad6182bb5e89aefaa76f116
Martinchox <maporollan@gmail.com>
2024-11-23 21:42:30 -0300

    Component PRODUCT

---
+++ src/components/AppProduct/FormProduct/FormProduct.tsx
@@ -0,0 +1,64 @@
+import { Button, Form } from "react-bootstrap"
+import { useForm } from "../../../hooks/useForm"
+import { FC } from "react";
+
+
+interface IPropsFormProduct {
+  handleAddProduct: Function;
+}
+
+export const FormProduct : FC<IPropsFormProduct> = ({handleAddProduct}) => {
+
+  const {handleChange, values, resetForm} = useForm(
+    {
+      nombre:'',
+      imagen:'',
+      precio:0,
+    }
+  )
+
+  const handleSumbitForm = ()=>{
+    handleAddProduct(values);
+    resetForm();
+
+  }
+
+
+
+  return (
+    <Form className="p-4 border rounded m-3">
+      <Form.Group controlId="formNombre">
+
+        <Form.Control type="text" name="nombre" placeholder="Ingrese nombre del producto" 
+        value={values.nombre} onChange={handleChange} /> 
+          
+        <Form.Label>Nombre</Form.Label>  
+
+      </Form.Group>
+
+      <Form.Group controlId="formImagen">
+
+      <Form.Control type="text" name="imagen" placeholder="Ingrese imagen del producto" 
+      value={values.imagen} onChange={handleChange} /> 
+  
+      <Form.Label>Imagen</Form.Label>  
+
+      </Form.Group>
+
+
+      <Form.Group controlId="formPrecio">
+
+      <Form.Control type="number" name="precio" placeholder="Ingrese el precio del producto" 
+      value={values.precio} onChange={handleChange} /> 
+  
+      <Form.Label>Precio</Form.Label>  
+
+      </Form.Group>
+
+      <div className="d-flex justify-content-center mt-4">
+        <Button onClick={handleSumbitForm} variant="primary">Enviar Producto</Button>
+      </div>
+      
+    </Form>
+  )
+}
